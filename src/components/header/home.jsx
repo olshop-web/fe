adfd42efb17687f7b702d13865a82f1ae4db7920
unknown <kakujaa0015@gmail.com>
2023-12-06 21:54:22 +0700

    head doang ah

---
+++ src/components/header/home.jsx
@@ -0,0 +1,12 @@
+import Menu from "./components/menu.jsx"
+import Search from "./components/search.jsx"
+const HeaderHome = () => {
+	return (
+		<div>
+			<Menu/>
+			<Search/>
+		</div>
+	);
+};
+
+export default HeaderHome;
